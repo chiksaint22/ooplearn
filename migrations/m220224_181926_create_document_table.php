752accbd7624c51e6558de58947fb2bccc1f8c4f
chiksaint <4MnpzPFqfQeJBrqJQnDl>
2022-02-28 18:20:46 +0300

    Загрузка файлов

---
+++ migrations/m220224_181926_create_document_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%document}}`.
+ */
+class m220224_181926_create_document_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%document}}', [
+            'id' => $this->primaryKey(),
+            'path'=>$this->string(200),
+            'user_id'=>$this->integer()
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%document}}');
+    }
+}
